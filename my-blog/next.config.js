64e96862fb83bb4ee1db10b124736cf82eabbc3c
Vishal Meena <61709884+Vishalmeenaa@users.noreply.github.com>
2024-09-09 09:16:24 -0700

    Update next.config.js

--- my-blog/next.config.js
+++ my-blog/next.config.js
@@ -1,17 +1,6 @@
-// @ts-check
-
-const { withContentlayer } = require("next-contentlayer");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  rewrites: async () => [
-    {
-      source: "/posts/test",
-      destination: "/posts/get-started",
-    },
-  ],
-};
+}
 
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig
